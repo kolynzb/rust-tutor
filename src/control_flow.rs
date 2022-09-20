d77a8fa0027b6b1412fb942cac3e78449c60d90e
kolynzb <collinsbenda360@gmail.com>
2022-09-20 10:59:45 +0300

    Complete control flow

--- src/control_flow.rs
+++ src/control_flow.rs
@@ -1,3 +1,55 @@
 pub fn run(){
-    
+    let number = 3;
+
+    // if statement
+    if number < 5 {
+    println!("condition was true");
+    } else {
+    println!("condition was false");
+    }
+
+    // else if
+        let condition = true;
+        let number = if condition {
+            5
+        } else {
+            6
+        };
+        println!("The value of number is: {}", number);
+        
+    // Loops.
+    loop {
+        println!("again!");
+    }
+
+    // Conditional
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    // While Loop 
+    fn main() {
+        let mut number = 3;
+        while number != 0 {
+        println!("{}!", number);
+        number = number - 1;
+        }
+        println!("LIFTOFF!!!");
+        }
+    // Loop through a collection 
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+        index = index + 1;
+    }
+    // safe
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
 }
